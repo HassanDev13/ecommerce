dea9056f54781891b6a4003d4ab170b230578995
hacene <zerrouk.mohammed.hacene@gmail.com>
2024-01-27 10:10:20 +0100

    Refactor prodect-order component

--- nextjs/src/app/(artisan)/artisan/[page]/prodects/components/prodect-order.tsx
+++ nextjs/src/app/(artisan)/artisan/[page]/prodects/components/prodect-order.tsx
@@ -7,9 +7,9 @@ import {
 } from "@/components/ui/sheet";
 import { useProductContext } from "../../../../../../../context/ProductContext";
 
-
 export const ProjectOrderDetails = () => {
-  const {  product  , isSheetProductOrdersOpen , setIsSheetProductOrdersOpen } = useProductContext();
+  const { product, isSheetProductOrdersOpen, setIsSheetProductOrdersOpen } =
+    useProductContext();
 
   return (
     <Sheet
@@ -22,6 +22,24 @@ export const ProjectOrderDetails = () => {
         </SheetHeader>
         <div className="flex flex-col justify-start items-center space-y-4">
           {product?.orders?.length}
+          {product?.orders.map((order) => (
+            <div
+              key={order.id}
+              className="h-50 w-full border flex justify-between rounded-xl p-4"
+            >
+              <div className="flex flex-col">
+                <p>
+                  {" "}
+                  {order.consumer.user.first_name +
+                    " " +
+                    order.consumer.user.last_name}
+                </p>
+                <p>{order.created_at}</p>
+                <p className="bg-green-200 p-1 w-fit rounded-lg">{order.order_status}</p>
+              </div>
+             
+            </div>
+          ))}
         </div>
       </SheetContent>
     </Sheet>
